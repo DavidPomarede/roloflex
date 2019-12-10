1d57ee97ee07972066c6d7e5bacb334d5d2e30a5
davidpom <davidpom@gmail.com>
2019-12-10 15:26:33 -0500

    no arg

--- contact.js
+++ contact.js
@@ -2,9 +2,16 @@ const program = require('commander');
 // Require logic.js file and extract controller functions using JS destructuring assignment
 const { addContact, getContact } = require('./logic');
 
+
 program
   .version('0.0.1')
   .description('Contact management system');
+
+program
+  .command('*')
+  .description('no arg')
+  .action(console.log("Arguments: \n a [FirstName LastName Phone Email] adds a contact\n r [queryString] retrieves a contact\n Command-c to quit.")
+);
 
 program
   .command('addContact <firstame> <lastname> <phone> <email>')
